7bba9cce576464d312f4acac1d2ecc06ca968319
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-08-31 16:52:04 -0300

    actualizo codigo de practico 7

--- practico7/Ej11NumerosPrimos.pas
+++ practico7/Ej11NumerosPrimos.pas
@@ -2,22 +2,22 @@ Program Ej11NumerosPrimos;
 Var number: Integer;
 // Programar EsPrimo() recibe un entero > 1, retorna true si es primo, false caso contrario
 
-Function EsPrimo(num: Integer): Boolean;
+Function IsPrimeNumber(num: Integer): Boolean;
 Var i : Integer;
 Begin
     { Se espera recibir un entero > 1 }
-    EsPrimo := True;
+    IsPrimeNumber := True;
 
     If num > 2 Then Begin
         For i := 2 To (num - 1) Do
-            If (num Mod i = 0) Then EsPrimo := False
+            If (num Mod i = 0) Then IsPrimeNumber := False
     End;
 End;
 
 Begin 
     Write('Ingresar numero: ');
     ReadLn(number);
-    If EsPrimo(number) Then 
+    If IsPrimeNumber(number) Then 
         WriteLn('El numero ', number, ' es primo')
     Else
         WriteLn('El numero ', number, ' no es primo')
