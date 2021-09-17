04f61336e1e65afc6f16e30017216dbd3fe437c2
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-09-16 21:12:14 -0300

    finalizo funcion riemannHasta

---
+++ Obligatorio1/tarea1.pas
@@ -0,0 +1,51 @@
+
+{ función f a la que queremos hallar su integral }
+Function f(x: Real): Real;
+Var
+    { argumentos del seno y la raíz }
+    argSeno, argRaiz : Real;
+Begin
+    argSeno := ((pi * x) + 25) / 10;
+    argRaiz := 2 + Sin(argSeno);
+    f := x * Sqrt(argRaiz)
+End;
+
+{ Dados los extremos izquierdo 'izq' y derecho 'der' del intervalo y la cantidad 
+'subintervalos' de rectángulos en que queremos dividir el área, calcula la Suma 
+de Riemann para la función f(x). 
+Precondiciones: subintervalos > 0 y izq < der. }
+Function sumaRiemann(izq, der : Real; subintervalos : Integer) : Real;
+Var 
+    i : Integer;
+    longitudParticion, valorX, suma : Real;
+Begin
+    suma := 0;
+    longitudParticion := Abs(der - izq) / subintervalos;
+
+    For i := 1 To subintervalos Do
+    Begin
+        valorX := izq + (longitudParticion * (i - 1));
+        suma := suma + (longitudParticion * f(valorX));
+    End;
+
+    sumaRiemann := suma;
+End;
+
+{ Dados los extremos izquierdo 'izq' y derecho 'der' del intervalo y un valor 'delta', retorna la 
+menor cantidad de subintervalos 'i', tal que la diferencia entre la Suma de Riemann para la función 
+f(x) con 'i' subintervalos y la Suma de Riemann para la función f(x) con 'i-1' sea menor que 'delta'. 
+Precondición: izq < der. }
+Function riemannHasta(izq, der : Real; delta : Real) : Integer;
+Var
+    diferencia : Real;
+    subintervalos : Integer;
+Begin
+    subintervalos := Trunc(der) - Trunc(izq) + 1;
+    Repeat
+        subintervalos := subintervalos -1 ;
+        diferencia := Abs(sumaRiemann(izq, der, subintervalos)) - Abs(sumaRiemann(izq, der, subintervalos - 1))
+
+    Until diferencia < delta;
+
+    riemannHasta := subintervalos;
+End;
