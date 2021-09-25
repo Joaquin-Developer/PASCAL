1f61eef6e6bf38ed3802e46cbbbc6642db533ff6
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-09-25 02:01:55 -0300

    agrego ejercicios de arreglos

---
+++ practico9/ej3.pas
@@ -0,0 +1,16 @@
+Program ej3;
+
+Const 
+    N = ...;
+Type 
+    Arreglo = Array [1..N] Of Integer;
+
+Procedure Carga(var arr: Arreglo);
+Begin
+
+End;
+
+Begin 
+
+
+End;
---
+++ practico9/ej4.pas
@@ -0,0 +1,27 @@
+Program ej4;
+
+Var
+    x : Integer;
+
+// Por Referencia: cambia el valor de la variable original
+Procedure PorReferenciaSetX(var x: Integer);
+Begin
+    x := 40;
+    WriteLn('En Set() x vale = ', x)
+End;
+
+// Por valor: no cambia el valor de la variable original
+Procedure PorValorSetX(x: Integer);
+Begin
+    x := 40;
+    WriteLn('En Set() x vale = ', x)
+End;
+
+Begin 
+    x := 16;
+    WriteLn('Por primera vez x vale = ', x);
+    // PorReferenciaSetX(x);
+    PorValorSetX(x);
+    WriteLn('AL final x vale = ', x);
+
+End.
