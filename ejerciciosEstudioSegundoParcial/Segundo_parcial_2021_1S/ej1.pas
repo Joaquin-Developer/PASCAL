8eccaf7b7143aaead654c1632be4635be8b86046
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-11-30 00:41:09 -0300

    ADD Ejercicio 1 Parcial 2021S1

--- ejerciciosEstudioSegundoParcial/Segundo_parcial_2021_1S/ej1.pas
+++ ejerciciosEstudioSegundoParcial/Segundo_parcial_2021_1S/ej1.pas
@@ -1,8 +1,11 @@
 Program ej1;
 
-type
-    Punto = record 
-                TipoConica = (circunferencia, elipse);
+Const MAX = 7; (* entero positivo cualquiera *)
+
+Type
+
+    TipoConica = (circunferencia, elipse);
+    Punto = record
                 x,y: Real;
             end;
 
@@ -15,29 +18,48 @@ type
                     figura : TipoConica;
                     enCentro : Boolean
                 end;
-
+ 
+    ArrConicas = array [1..MAX] of Conica;
+ 
 
 // parte a) (15 pts) Escribir el procedimiento focoEnCentro que devuelve en el parámetro resultado el tipo de cónica y el valor
 // booleano true si la cónica tiene alguno de sus focos en el centro de ejes cartesianos (0.0, 0.0), 
 // o false si no. En el caso de una circunferencia, su centro es el foco.
-Procedure focoEnCentro (conica : Conica; Var resultado : TipoResul);
+Procedure focoEnCentro(conica : Conica; Var resultado : TipoResul);
 Begin 
     resultado.figura := conica.clase;
     
     // Si es cfa:
-    If resultado.figura = circunferencia Then 
+    If (resultado.figura = circunferencia) And (conica.centro.x = 0) And (conica.centro.y = 0) Then 
+        resultado.enCentro := True
+    Else If (resultado.figura = elipse) Then
     Begin 
-
-    End; 
-
     // Si es elipse:
-    If resultado.figura = circunferencia Then 
-    Begin 
-    End;
-
+        If ((conica.foco1.x = 0) And (conica.foco1.y = 0)) Or ((conica.foco2.x = 0) And (conica.foco2.y = 0)) Then 
+            resultado.enCentro := True;
+    End
+    Else
+        resultado.enCentro := False; 
+    
 End;
 
+// Parte b) (15 puntos) Dado un arreglo de cónicas
+// Escribir la función primeraCfa que devuelve el índice del primer elemento del arreglo que 
+// es circunferencia o -1 si no hay ninguna
+Function primeraCfa(conicas : ArrConicas): Integer; 
+Var 
+    i, indice : Integer;
+Begin 
+    indice := -1;
+    i := 1;
 
+    While (indice = -1) And (i <= MAX) Do 
+        If conicas[i].clase = circunferencia Then 
+            indice := i;
 
+    primeraCfa := indice
+End;
+
+// Bloque principal:
 Begin
 End.
